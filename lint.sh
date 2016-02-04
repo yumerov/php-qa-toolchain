bc8037719d32e0b90e74c3c7d141360e0c40a153
Levent Yumerov <yumerov.levent@gmail.com>
2016-02-04 12:32:40 +0200

    add input parameters to lint.sh and checks if they are valid

--- lint.sh
+++ lint.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
-# usage: lint.sh
+# usage: lint.sh <SOURCE_DIRECTORY> <INTERVAL>
 
-declare -r INTERVAL=1;
-declare -r SOURCE_DIR="lint-files/";
+declare -r USAGE="USAGE: bash lint.sh <SOURCE_DIRECTORY> <INTERVAL>";
+declare -r EXAMPLE="EXAMPLE: bash lint.sh src/ 5";
+declare -r SOURCE_DIR=$1;
+declare -r INTERVAL=$2;
+declare error_output;
 
-watch -n $INTERVAL "
-  echo \"PHP Active Linter by Levent \\\"lzy\\\" Yumerov\";
-  find $SOURCE_DIR -type f -name \"*.php\" \
-    | xargs -n1 php --syntax-check \
-    | grep \"^(No syntax errors detected in)\";
-  clear;"
+if [ "$SOURCE_DIR" ];
+then
+  if [ -d "$SOURCE_DIR" ];
+  then
+    if [ "$INTERVAL" ];
+    then
+      if [[ "$INTERVAL" =~ ^[0-9]+$ ]];
+      then
+        watch -n $INTERVAL "
+          echo \"PHP Active Linter by Levent \\\"lzy\\\" Yumerov\";
+          find $SOURCE_DIR -type f -name \"*.php\" \
+            | xargs -n1 php --syntax-check \
+            | grep \"^(No syntax errors detected in)\";
+          clear;";
+      else
+        error_output="Check interval must be a number.";
+      fi
+    else
+      error_output="Check interval is not set.";
+    fi
+  else
+    error_output="Directory \"${SOURCE_DIR}\" does not exist.";
+  fi
+else
+  error_output="Source directory is not set.";
+fi
+
+if [ "$error_output" ];
+then
+  echo -e "${error_output}\n${USAGE}\n${EXAMPLE}";
+fi
